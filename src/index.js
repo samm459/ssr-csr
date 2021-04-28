a69b8b1e86488fd9b4c469cef3a926521b361e0b
samm459 <sam@contentcucumber.com>
2021-04-28 09:54:50 -0400

    Setup examples

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const Express = require("express");
+
+const app = Express();
+
+/**
+ * An example of "server side" rendering
+ */
+app.get("/ssr", (_, res) => {
+  const text = "Hello World";
+  res.end(`<h1>${text}</h1>`);
+});
+
+/**
+ * An example of "client side" rendering
+ */
+app.get("/csr", (_, res) => {
+  res.end(
+    `<h1 id="app"></h1>
+    <script>
+      const text = document.createTextNode("Hello World"); 
+      document.getElementById("app").appendChild(text)
+    </script>`
+  );
+});
+
+app.listen(8090);
